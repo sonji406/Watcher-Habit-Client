018e36afa60a8c821bf183f320bd473765b52123
hamsterster <xhrrl003@gmail.com>
2023-09-12 03:17:29 +0900

    Feat: 로고 폰트 및 크기 적용

---
+++ src/components/MainPage/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="text-7xl" style={{ fontFamily: "Sinoreta" }}>
+      WatcherHabit
+    </div>
+  );
+};
+
+export default Logo;
