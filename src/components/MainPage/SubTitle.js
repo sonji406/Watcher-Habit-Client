66a1f68c307ad6613d19c81db16321f82ba91e33
hamsterster <xhrrl003@gmail.com>
2023-09-12 03:17:55 +0900

    Feat: 서브타이틀 폰트 및 크기 적용

---
+++ src/components/MainPage/SubTitle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SubTitle = () => {
+  return (
+    <div className="text-xl mb-8" style={{ fontFamily: "NotoSans" }}>
+      새로운 습관을 만들어보세요
+    </div>
+  );
+};
+
+export default SubTitle;
