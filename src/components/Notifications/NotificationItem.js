4ddc0577d0d0d83edeb6e44acfa3db3d8f5bb26b
hamsterster <xhrrl003@gmail.com>
2023-09-13 06:44:53 +0900

    Feat: 알림 닫기 기능 및 이미지 url 반영

--- src/components/Notifications/NotificationItem.js
+++ src/components/Notifications/NotificationItem.js
@@ -1,17 +1,71 @@
 import React from 'react';
 
-const NotificationItem = ({ message, date, time }) => {
+const NotificationItem = ({
+  message,
+  date,
+  time,
+  status,
+  isVisible,
+  setIsVisible,
+}) => {
+  const renderButton = () => {
+    switch (status) {
+      case 'success':
+        return (
+          <button className='bg-[#1C2532] text-white text-sm mt-2 px-4 py-2 rounded-full'>
+            확인하러 가기
+          </button>
+        );
+      case 'failure':
+        return (
+          <button className='bg-[#1C2532] text-white text-sm mt-2 px-4 py-2 rounded-full'>
+            확인하러 가기
+          </button>
+        );
+      case 'verificationRequest':
+        return (
+          <button className='bg-[#1C2532] text-white text-sm mt-2 px-4 py-2 rounded-full'>
+            인증하러 가기
+          </button>
+        );
+      case 'approveRequest':
+        return (
+          <button className='bg-[#1C2532] text-white text-sm mt-2 px-4 py-2 rounded-full'>
+            승인하러 가기
+          </button>
+        );
+      case 'invite':
+        return (
+          <button className='bg-[#1C2532] text-white text-sm mt-2 px-4 py-2 rounded-full'>
+            수락하기
+          </button>
+        );
+      default:
+        return (
+          <button className='bg-[#1C2532] text-white text-sm mt-2 px-4 py-2 rounded-full'>
+            확인하러 가기
+          </button>
+        );
+    }
+  };
+
   return (
-    <div className='py-2 px-4 flex flex-col items-center justify-cente rounded-lg bg-[#27313C] m-4'>
-      <div className='self-end flex space-x-1'>
-        <p className='text-sm text-gray-600'>{date}</p>
-        <p className='text-sm text-gray-600'>{time}</p>
+    isVisible && (
+      <div className='py-2 px-4 flex flex-col items-center justify-center rounded-lg bg-[#27313C] m-4 relative'>
+        <div className='absolute top-0 right-3 flex space-x-1'>
+          <p className='text-sm text-gray-600'>{date}</p>
+          <p className='text-sm text-gray-600'>{time}</p>
+          <button
+            onClick={() => setIsVisible(false)}
+            className='text-blue-500 text-xs'
+          >
+            닫기
+          </button>
+        </div>
+        <p className='text-center mt-5'>{message}</p>
+        {renderButton()}
       </div>
-      <p className='text-center'>{message}</p>
-      <button className='bg-black text-white text-sm mt-2 px-4 py-2 rounded-full'>
-        확인하러 가기
-      </button>
-    </div>
+    )
   );
 };
 
--- src/components/Notifications/NotificationList.js
+++ src/components/Notifications/NotificationList.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import NotificationItem from './NotificationItem';
+import bellIcon from './bell.png';
 
-const NotificationList = ({ notifications }) => {
+const NotificationList = ({ notifications, setNotifications }) => {
+  const setIsVisible = (id, value) => {
+    setNotifications(
+      notifications.map((notification) =>
+        notification.id === id
+          ? { ...notification, isVisible: value }
+          : notification,
+      ),
+    );
+  };
+
+  const closeAllNotifications = () => {
+    setNotifications(
+      notifications.map((notification) => ({
+        ...notification,
+        isVisible: false,
+      })),
+    );
+  };
+
   return (
     <div className='bg-[#1C2532] rounded-lg'>
-      <div className='flex justify-between p-2'>
-        <p className='font-bold'>알림</p>
-        <button className='text-sm text-gray-400'>지우기</button>
+      <div className='flex justify-between p-2 items-center'>
+        <div className='flex items-center'>
+          <img src={bellIcon} alt='bell icon' className='h-4 w-4 mr-2' />
+          <p className='text-[26A34F] font-bold'>알림</p>
+        </div>
+        <button
+          className='text-sm text-gray-400'
+          onClick={closeAllNotifications}
+        >
+          전체 알림 닫기
+        </button>
       </div>
       {notifications.map((notification, index) => (
-        <NotificationItem key={index} {...notification} />
+        <NotificationItem
+          key={index}
+          message={notification.message}
+          date={notification.date}
+          time={notification.time}
+          status={notification.status}
+          isVisible={notification.isVisible}
+          setIsVisible={(value) => setIsVisible(notification.id, value)}
+        />
       ))}
       <button className='w-full text-center py-2 text-sm text-red-400'>
         로그아웃
