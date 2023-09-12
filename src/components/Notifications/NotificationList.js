60b33947b71378da446d5ba42add3b0d9d0aa3c0
hamsterster <xhrrl003@gmail.com>
2023-09-13 06:52:32 +0900

    Feat: 알림창 리스트 스크롤 추가

--- src/components/Notifications/NotificationList.js
+++ src/components/Notifications/NotificationList.js
@@ -23,7 +23,7 @@ const NotificationList = ({ notifications, setNotifications }) => {
   };
 
   return (
-    <div className='bg-[#1C2532] rounded-lg'>
+    <div className='bg-[#1C2532] rounded-lg h-[80vh] flex flex-col'>
       <div className='flex justify-between p-2 items-center'>
         <div className='flex items-center'>
           <img src={bellIcon} alt='bell icon' className='h-4 w-4 mr-2' />
@@ -36,20 +36,24 @@ const NotificationList = ({ notifications, setNotifications }) => {
           전체 알림 닫기
         </button>
       </div>
-      {notifications.map((notification, index) => (
-        <NotificationItem
-          key={index}
-          message={notification.message}
-          date={notification.date}
-          time={notification.time}
-          status={notification.status}
-          isVisible={notification.isVisible}
-          setIsVisible={(value) => setIsVisible(notification.id, value)}
-        />
-      ))}
-      <button className='w-full text-center py-2 text-sm text-red-400'>
-        로그아웃
-      </button>
+      <div className='overflow-y-auto flex-grow'>
+        {notifications.map((notification, index) => (
+          <NotificationItem
+            key={index}
+            message={notification.message}
+            date={notification.date}
+            time={notification.time}
+            status={notification.status}
+            isVisible={notification.isVisible}
+            setIsVisible={(value) => setIsVisible(notification.id, value)}
+          />
+        ))}
+      </div>
+      <div className='flex-shrink-0'>
+        <button className='w-full text-center py-2 text-sm text-red-400'>
+          로그아웃
+        </button>
+      </div>
     </div>
   );
 };
