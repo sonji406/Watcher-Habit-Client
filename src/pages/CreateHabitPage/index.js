030bcc2e738d1c12f4df2edcdf6d491b976078b2
hamsterster <xhrrl003@gmail.com>
2023-09-14 15:32:17 +0900

    Feat: 그룹 선택 조건부 렌더링 및 useEffect 수정

--- src/pages/CreateHabitPage/index.js
+++ src/pages/CreateHabitPage/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import Header from '../../components/Common/Header';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Header from '../../components/common/Header';
 
 const mockCreateHabitAPI = (data) => {
   return new Promise((resolve) => {
@@ -31,7 +32,23 @@ const mockEditHabitAPI = (data) => {
   });
 };
 
+const mockGetGroupsAPI = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        status: 200,
+        data: [
+          { groupId: 'group1', groupName: 'Group 1' },
+          { groupId: 'group2', groupName: 'Group 2' },
+          { groupId: 'group3', groupName: 'Group 3' },
+        ],
+      });
+    }, 500);
+  });
+};
+
 const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+const minuteOptions = ['00', '15', '30', '45'];
 
 const CreateOrEditHabit = ({ isEdit = false }) => {
   const today = new Date().toISOString().split('T')[0];
@@ -40,15 +57,17 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
   const [habitStartDate, setHabitStartDate] = useState(today);
   const [habitEndDate, setHabitEndDate] = useState('');
   const [doDay, setDoDay] = useState([]);
-  const [startTime, setStartTime] = useState('00:00');
+  const [startTime, setStartTime] = useState('01:00');
   const [duration, setDuration] = useState(0);
   const [timePeriod, setTimePeriod] = useState('AM');
   const [penalty, setPenalty] = useState('');
   const [minApprovalCount, setMinApprovalCount] = useState(0);
   // const [creator, setCreator] = useState('');
-  // const [sharedGroup, setSharedGroup] = useState([]);
+  const [sharedGroup, setSharedGroup] = useState(null);
+  const [groupOptions, setGroupOptions] = useState([]);
+  const [submitFlag, setSubmitFlag] = useState(false);
 
-  const minuteOptions = ['00', '15', '30', '45'];
+  const navigate = useNavigate();
 
   const hourOptions =
     timePeriod === 'AM'
@@ -59,53 +78,92 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
 
   const isFormValid = habitTitle && habitStartDate && habitEndDate;
 
-  const handleSubmit = async () => {
-    if (!isFormValid) return;
-
-    let calculatedStartTime;
-    let calculatedEndTime;
-
-    if (startTime && timePeriod) {
-      let [inputHour] = startTime.split(':');
-      inputHour = parseInt(inputHour);
-      if (timePeriod === 'PM' && inputHour < 12) inputHour += 12;
-      if (timePeriod === 'AM' && inputHour === 12) inputHour = 0;
-      calculatedStartTime = `${String(inputHour).padStart(2, '0')}:${
-        startTime.split(':')[1]
-      }`;
-    }
-
-    const habitData = {
-      habitTitle,
-      habitContent,
-      habitStartDate,
-      habitEndDate,
-      doDay,
-      startTime: calculatedStartTime,
-      endTime: calculatedEndTime,
-      penalty,
-      // creator,
-      // sharedGroup,
-      minApprovalCount,
+  useEffect(() => {
+    const fetchGroups = async () => {
+      try {
+        const response = await mockGetGroupsAPI();
+        if (response.status === 200) {
+          setGroupOptions(response.data);
+        }
+      } catch (error) {
+        console.error('Could not fetch groups:', error);
+      }
     };
 
-    try {
-      let response;
-      if (isEdit) {
-        response = await mockEditHabitAPI(habitData);
-      } else {
-        response = await mockCreateHabitAPI(habitData);
+    fetchGroups();
+  }, []);
+
+  const handleSubmit = () => {
+    if (!isFormValid) return;
+    setSubmitFlag(!submitFlag);
+  };
+
+  useEffect(() => {
+    const submitHabit = async () => {
+      if (!isFormValid) return;
+
+      let calculatedStartTime;
+      let calculatedEndTime;
+
+      if (startTime && timePeriod) {
+        let [inputHour, inputMinute] = startTime.split(':').map(Number);
+
+        if (timePeriod === 'PM' && inputHour < 12) inputHour += 12;
+        if (timePeriod === 'AM' && inputHour === 12) inputHour = 0;
+
+        calculatedStartTime = `${String(inputHour).padStart(2, '0')}:${String(
+          inputMinute,
+        ).padStart(2, '0')}`;
+
+        const endDateTime = new Date();
+        endDateTime.setHours(inputHour, inputMinute);
+        endDateTime.setMinutes(endDateTime.getMinutes() + duration);
+
+        const endHour = endDateTime.getHours();
+        const endMinute = endDateTime.getMinutes();
+
+        calculatedEndTime = `${String(endHour).padStart(2, '0')}:${String(
+          endMinute,
+        ).padStart(2, '0')}`;
       }
 
-      if (response.status === 200 || response.status === 201) {
-        console.log('Success:', response.data);
-      } else {
-        console.log('Error:', response.data);
+      const habitData = {
+        habitTitle,
+        habitContent,
+        habitStartDate,
+        habitEndDate,
+        doDay,
+        startTime: calculatedStartTime,
+        endTime: calculatedEndTime,
+        penalty,
+        // creator,
+        sharedGroup,
+        minApprovalCount,
+      };
+
+      try {
+        let response;
+        if (isEdit) {
+          response = await mockEditHabitAPI(habitData);
+        } else {
+          response = await mockCreateHabitAPI(habitData);
+        }
+
+        if (response.status === 200 || response.status === 201) {
+          console.log('Success:', response.data);
+        } else {
+          console.log('Error:', response.data);
+        }
+      } catch (error) {
+        console.error('API Error:', error);
       }
-    } catch (error) {
-      console.error('API Error:', error);
+    };
+
+    if (isFormValid) {
+      submitHabit();
     }
-  };
+    // eslint-disable-next-line
+  }, [submitFlag]);
 
   const toggleDay = (day) => {
     setDoDay((prevDoDay) => {
@@ -160,7 +218,6 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
           </div>
 
           <label>기한</label>
-
           <div className='grid grid-cols-2 gap-4 mb-4'>
             <input
               className='p-2 border rounded'
@@ -193,7 +250,6 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
             </button>
 
             <span>매주</span>
-
             {daysOfWeek.map((day) => (
               <button
                 key={day}
@@ -236,6 +292,7 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
                 ))}
               </select>
               <span>시</span>
+
               <select
                 className='p-2 border rounded'
                 value={startTime.split(':')[1]}
@@ -253,6 +310,7 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
               </select>
               <span>분 부터</span>
             </div>
+
             <div className='flex space-x-2'>
               <select
                 className='p-2 border rounded'
@@ -266,6 +324,7 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
                 ))}
               </select>
               <span>시간</span>
+
               <select
                 className='p-2 border rounded'
                 value={duration % 60}
@@ -285,6 +344,24 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
             </div>
           </div>
 
+          <label>그룹 선택</label>
+          <div className='mb-4'>
+            <select
+              className='w-full p-2 border rounded'
+              value={sharedGroup || ''}
+              onChange={(e) => setSharedGroup(e.target.value)}
+            >
+              <option value=''>
+                초대된 그룹만 선택 가능합니다(미선택 시 비공개 처리됩니다)
+              </option>
+              {groupOptions.map((group) => (
+                <option key={group.groupId} value={group.groupId}>
+                  {group.groupName}
+                </option>
+              ))}
+            </select>
+          </div>
+
           <label>패널티 내용</label>
           <div className='mb-4'>
             <textarea
@@ -298,25 +375,28 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
             />
           </div>
 
-          <label>최소 승인 인원</label>
-          <div className='mb-4 flex items-center'>
-            <button
-              className='bg-gray-300 hover:bg-gray-400 text-black font-bold py-1 px-2 rounded'
-              onClick={() =>
-                setMinApprovalCount(Math.max(minApprovalCount - 1, 0))
-              }
-            >
-              -
-            </button>
-            <span className='px-4'>{minApprovalCount}</span>
-            <button
-              className='bg-gray-300 hover:bg-gray-400 text-black font-bold py-1 px-2 rounded'
-              onClick={() => setMinApprovalCount(minApprovalCount + 1)}
-            >
-              +
-            </button>
-          </div>
-
+          {sharedGroup && (
+            <>
+              <label>최소 승인 인원</label>
+              <div className='mb-4 flex items-center'>
+                <button
+                  className='bg-gray-300 hover:bg-gray-400 text-black font-bold py-1 px-2 rounded'
+                  onClick={() =>
+                    setMinApprovalCount(Math.max(minApprovalCount - 1, 0))
+                  }
+                >
+                  -
+                </button>
+                <span className='px-4'>{minApprovalCount}</span>
+                <button
+                  className='bg-gray-300 hover:bg-gray-400 text-black font-bold py-1 px-2 rounded'
+                  onClick={() => setMinApprovalCount(minApprovalCount + 1)}
+                >
+                  +
+                </button>
+              </div>
+            </>
+          )}
           <div className='flex justify-between mt-6'>
             <button
               className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
@@ -328,7 +408,7 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
             <button
               className='bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded'
               onClick={() => {
-                /* 캔슬 시 이전 페이지로 이동 예정 */
+                navigate(-1);
               }}
             >
               Cancel
