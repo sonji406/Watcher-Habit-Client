dad8413fac7107888fc7e7a01653d03136ab605a
hamsterster <xhrrl003@gmail.com>
2023-09-15 12:42:29 +0900

    Feat: 그룹선택 물음표 호버 시 설명 나타나도록 구현

--- src/pages/CreateHabit/index.js
+++ src/pages/CreateHabit/index.js
@@ -20,8 +20,6 @@ const getUserIdFromToken = () => {
 
 const userId = getUserIdFromToken();
 
-console.log(userId);
-
 const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
 const minuteOptions = ['00', '15', '30', '45'];
 
@@ -275,7 +273,41 @@ const CreateOrEditHabit = ({ isEdit = false }) => {
             </div>
           </div>
 
-          <label>그룹 선택</label>
+          <label className='relative'>
+            그룹 선택
+            <span className='ml-2 inline-block group cursor-pointer'>
+              <span className='bg-gray-200 rounded-full pr-2 pl-2'>?</span>
+              <span
+                className='absolute invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-opacity p-4 rounded border text-sm'
+                style={{
+                  left: '100%',
+                  top: '50%',
+                  transform: 'translateY(-50%)',
+                  zIndex: 10,
+                  minWidth: '300px',
+                  backgroundColor: '#f2f2f2',
+                }}
+              >
+                <p className='mb-2'>
+                  <span className='text-black'>그룹 선택이란?</span>
+                </p>
+                <p className='mb-2'>
+                  <span className='text-green-500'>그룹 미선택 시</span>
+                  <p>
+                    비공개 처리 되며, 나의 습관 관리 페이지에서 본인만 확인
+                    가능한 습관이 생성됩니다.
+                  </p>
+                </p>
+                <p>
+                  <span className='text-blue-500'>그룹 선택 시</span>
+                  <p>
+                    해당 그룹 페이지에 공유 되며, 나의 습관 관리 페이지에서도
+                    확인 가능한 습관이 생성됩니다.
+                  </p>
+                </p>
+              </span>
+            </span>
+          </label>
           <div className='mb-4'>
             <select
               className='w-full p-2 border rounded'
