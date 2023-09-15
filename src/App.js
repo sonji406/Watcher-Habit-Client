544a1a0c3864bfb87927f6c47e9045d2e17e991d
sonji406 <wkaqh24@gmail.com>
2023-09-15 17:59:38 +0900

    Feat: 사이드바 라우트 적용

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import CreateHabit from './pages/CreateHabit';
 import MyHabit from './pages/MyHabit';
 import Group from './pages/Group';
 import AwsS3Test from './pages/AwsS3Test';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
@@ -17,12 +18,24 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/create-nickname' element={<CreateNickname />} />
           <Route
-            path='/my-habit/:nickname/new-habit'
-            element={<CreateHabit />}
+            path='*'
+            element={
+              <div className='flex'>
+                <Sidebar />
+                <div className='flex-1'>
+                  <Routes>
+                    <Route
+                      path='/my-habit/:nickname/new-habit'
+                      element={<CreateHabit />}
+                    />
+                    <Route path='/my-habit/:nickname' element={<MyHabit />} />
+                    <Route path='/group/:groupId' element={<Group />} />
+                    <Route path='/aws-s3-test' element={<AwsS3Test />} />
+                  </Routes>
+                </div>
+              </div>
+            }
           />
-          <Route path='/my-habit/:nickname' element={<MyHabit />} />
-          <Route path='/group/:groupId' element={<Group />} />
-          <Route path='/aws-s3-test' element={<AwsS3Test />} />
         </Routes>
       </Router>
     </Provider>
