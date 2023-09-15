2b67744f71cc82534d12ab1fb39a361d418db196
hamsterster <xhrrl003@gmail.com>
2023-09-15 11:22:27 +0900

    Refactor: 토큰에서 userId 받는 로직 분리 및 리팩토링

--- src/services/api/userGet.js
+++ src/services/api/userGet.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
-export const getUserProfileImage = async (userId) => {
+export const getUserInfo = async (userId) => {
   try {
     const response = await axios.get(
       `${process.env.REACT_APP_SERVER_DOMAIN}/api/user/${userId}`,
     );
-    const user = response.data;
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserProfileImage = async (userId) => {
+  try {
+    const user = await getUserInfo(userId);
     return user.profileImageUrl;
   } catch (error) {
     throw error;
