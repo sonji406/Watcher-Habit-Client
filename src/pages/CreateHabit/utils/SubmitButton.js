577c4368667db4558ea2987729fcb9b9de062f9d
hamsterster <xhrrl003@gmail.com>
2023-09-15 23:55:08 +0900

    Refactor: 리뷰 반영 누락된 isLoading 적용

--- src/pages/CreateHabit/utils/SubmitButton.js
+++ src/pages/CreateHabit/utils/SubmitButton.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const SubmitButton = ({ isEdit, handleSubmit }) => {
+const SubmitButton = ({ isEdit, isLoading, handleSubmit }) => {
   const buttonText = isEdit ? 'Edit' : 'Create';
 
   return (
     <button
-      className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+      disabled={isLoading}
+      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+        isLoading ? 'cursor-not-allowed' : ''
+      }`}
       onClick={handleSubmit}
     >
-      {buttonText}
+      {isLoading ? 'Loading...' : buttonText}
     </button>
   );
 };
