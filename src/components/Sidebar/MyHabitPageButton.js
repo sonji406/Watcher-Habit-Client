84c733fbfa66d904b01e11988d0fb2364e2cc987
sonji406 <wkaqh24@gmail.com>
2023-09-16 00:30:00 +0900

    Refactor: 불필요한 태그 제거

--- src/components/Sidebar/MyHabitPageButton.js
+++ src/components/Sidebar/MyHabitPageButton.js
@@ -3,21 +3,19 @@ import MyHabitIcon from './icon/MyHabit';
 
 const MyHabitPageButton = ({ nickname, isHovered, isCurrentPage }) => {
   return (
-    <div className='mb-14'>
-      <Link to={`/my-habit/${nickname}`}>
-        <div
-          className={`flex items-center text-customGray hover:text-customDarkGray hover:font-bold p-3 ${
-            isCurrentPage
-              ? 'bg-dark-green-bg rounded-full text-customGreen hover:text-customGreen'
-              : ''
-          } ${isHovered ? 'w-[210px] ' : ''}`}
-          style={{ cursor: 'pointer' }}
-        >
-          <MyHabitIcon />
-          {isHovered && <span className='ml-2'>나의 습관 관리</span>}
-        </div>
-      </Link>
-    </div>
+    <Link to={`/my-habit/${nickname}`} className='mb-14'>
+      <div
+        className={`flex items-center text-customGray hover:text-customDarkGray hover:font-bold p-3 ${
+          isCurrentPage
+            ? 'bg-dark-green-bg rounded-full text-customGreen hover:text-customGreen'
+            : ''
+        } ${isHovered ? 'w-[210px] ' : ''}`}
+        style={{ cursor: 'pointer' }}
+      >
+        <MyHabitIcon />
+        {isHovered && <span className='ml-2'>나의 습관 관리</span>}
+      </div>
+    </Link>
   );
 };
 
