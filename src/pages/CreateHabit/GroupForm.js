f1663dc20b71d2e9fa0f0e637e08a4b4e0877848
hamsterster <xhrrl003@gmail.com>
2023-09-15 18:33:28 +0900

    Feat: 툴팁 추가 구현

--- src/pages/CreateHabit/GroupForm.js
+++ src/pages/CreateHabit/GroupForm.js
@@ -1,43 +1,39 @@
 import React from 'react';
+import Tooltip from './Tooltip';
 
 const GroupForm = ({ sharedGroup, setSharedGroup, groupOptions }) => {
+  const tooltipText = (
+    <>
+      <div className='mb-2'>
+        <span className='text-black'>그룹 선택이란?</span>
+      </div>
+      <div className='mb-2'>
+        <span className='text-green-500'>그룹 미선택 시</span>
+        <div>
+          비공개 처리 되며, 나의 습관 관리 페이지에서 본인만 확인 가능한 습관이
+          생성됩니다.
+        </div>
+      </div>
+      <div>
+        <span className='text-blue-500'>그룹 선택 시</span>
+        <div>
+          해당 그룹 페이지에 공유 되며, 나의 습관 관리 페이지에서도 확인 가능한
+          습관이 생성됩니다.
+        </div>
+      </div>
+    </>
+  );
+
   return (
     <div>
       <label className='relative'>
         그룹 선택
         <span className='ml-2 inline-block group cursor-pointer'>
           <span className='bg-gray-200 rounded-full pr-2 pl-2'>?</span>
-          <span
-            className='absolute invisible group-hover:visible opacity-0 group-hover:opacity-100 transition-opacity p-4 rounded border text-sm'
-            style={{
-              left: '100%',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              zIndex: 10,
-              minWidth: '300px',
-              backgroundColor: '#f2f2f2',
-            }}
-          >
-            <div className='mb-2'>
-              <span className='text-black'>그룹 선택이란?</span>
-            </div>
-            <div className='mb-2'>
-              <span className='text-green-500'>그룹 미선택 시</span>
-              <div>
-                비공개 처리 되며, 나의 습관 관리 페이지에서 본인만 확인 가능한
-                습관이 생성됩니다.
-              </div>
-            </div>
-            <div>
-              <span className='text-blue-500'>그룹 선택 시</span>
-              <div>
-                해당 그룹 페이지에 공유 되며, 나의 습관 관리 페이지에서도 확인
-                가능한 습관이 생성됩니다.
-              </div>
-            </div>
-          </span>
+          <Tooltip text={tooltipText} />
         </span>
       </label>
+
       <div className='mb-4'>
         <select
           className='w-full p-2 border rounded'
--- src/pages/CreateHabit/PenaltyForm.js
+++ src/pages/CreateHabit/PenaltyForm.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Tooltip from './Tooltip';
 
 const PenaltyForm = ({ penalty, setPenalty }) => {
   return (
     <div>
-      <label>패널티 내용</label>
+      <label className='relative'>
+        패널티 내용
+        <span className='ml-2 inline-block group cursor-pointer'>
+          <span className='bg-gray-200 rounded-full pr-2 pl-2'>?</span>
+          <Tooltip text='동기부여를 위해 패널티를 입력해야 합니다.' />
+        </span>
+      </label>
+
       <div className='mb-4'>
         <textarea
           className='w-full p-2 border rounded'
