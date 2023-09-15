ce25a53eeb3025a54aaf6ebee33fe79fbe166631
Josh5519 <AcForCompany@gmail.com>
2023-09-15 23:12:46 +0900

    Refactor: 내 습관 페이지 데이터 api 호출해서 가져오게 수정

--- src/pages/MyHabit/index.js
+++ src/pages/MyHabit/index.js
@@ -1,5 +1,8 @@
 import HabitList from '../../components/habits/HabitList';
 import HabitDetail from '../../components/habits/HabitDetail';
+import { useParams } from 'react-router-dom';
+import getCurrentDate from '../../utils/getCurrentDate';
+import { useDailyHabits } from '../../hooks/useDailyHabits';
 
 const mockResponseData = {
   status: 200,
@@ -112,19 +115,19 @@ const mockResponseData = {
 };
 
 function MyHabit() {
-  // const { nickname } = useParams('nickname');
-  // const currentDate = getCurrentDate();
-  // const { dailyHabits, loading, error } = useDailyHabits(
-  //   `api/group/${nickname}/habitList?date=${currentDate}`,
-  // );
+  const { nickname } = useParams('nickname');
+  const currentDate = getCurrentDate();
+  const { dailyHabits, loading, error } = useDailyHabits(
+    `${process.env.REACT_APP_SERVER_DOMAIN}/api/user/${nickname}/habitList?date=${currentDate}`,
+  );
 
-  // if (loading) return <div>Loading...</div>;
-  // if (error) return <div>Error: {error.message}</div>;
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div className='min-h-screen flex flex-col bg-main-bg text-white bg-vignette'>
       <div className='w-full h-4/5 flex'>
-        <HabitList dailyHabits={mockResponseData}></HabitList>
+        <HabitList dailyHabits={dailyHabits}></HabitList>
         <HabitDetail></HabitDetail>
       </div>
     </div>
