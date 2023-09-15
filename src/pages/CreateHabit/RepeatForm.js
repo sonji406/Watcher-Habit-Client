1b4baf21dbcfade5871f2950912dce0413ef1aa6
hamsterster <xhrrl003@gmail.com>
2023-09-15 14:10:41 +0900

    Fix: 요일 선택 시 handlesubmit 호출되던 버그 해결

--- src/pages/CreateHabit/RepeatForm.js
+++ src/pages/CreateHabit/RepeatForm.js
@@ -3,21 +3,34 @@ import React from 'react';
 const RepeatForm = ({ doDay, setDoDay }) => {
   const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
 
+  const toggleDay = (day) => {
+    setDoDay((prevDoDay) => {
+      if (prevDoDay.includes(day)) {
+        return prevDoDay.filter((d) => d !== day);
+      } else {
+        return [...prevDoDay, day];
+      }
+    });
+  };
+
+  const toggleAllDays = () => {
+    if (doDay.length === 7) {
+      setDoDay([]);
+    } else {
+      setDoDay(daysOfWeek);
+    }
+  };
+
   return (
     <div>
       <label>반복 주기</label>
       <div className='mb-4 flex justify-between'>
         <button
+          type='button'
           className={`py-2 px-4 border rounded ${
             doDay.length === 7 ? 'bg-green-bg' : ''
           }`}
-          onClick={() => {
-            if (doDay.length === 7) {
-              setDoDay([]);
-            } else {
-              setDoDay(daysOfWeek);
-            }
-          }}
+          onClick={toggleAllDays}
         >
           매일
         </button>
@@ -25,18 +38,13 @@ const RepeatForm = ({ doDay, setDoDay }) => {
         <span>매주</span>
         {daysOfWeek.map((day) => (
           <button
+            type='button'
             key={day}
             className={`py-2 px-4 border rounded ${
               doDay.includes(day) ? 'bg-green-bg' : ''
             }`}
             onClick={() => {
-              setDoDay((prevDoDay) => {
-                if (prevDoDay.includes(day)) {
-                  return prevDoDay.filter((d) => d !== day);
-                } else {
-                  return [...prevDoDay, day];
-                }
-              });
+              toggleDay(day);
             }}
           >
             {day}
