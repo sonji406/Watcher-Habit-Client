5f0261647e4db8cca447387a623bcde8087a25fc
hamsterster <xhrrl003@gmail.com>
2023-09-18 08:17:54 +0900

    Chore: 그룹 텍스트 수정

--- src/components/habits/HabitDetail/AuthorAndVisibility.js
+++ src/components/habits/HabitDetail/AuthorAndVisibility.js
@@ -2,9 +2,9 @@ const AuthorAndVisibility = ({ creator, sharedGroup }) => {
   return (
     <div className='mb-4 text-right'>
       작성자 {creator.nickname}
-      <div className='text-sm'>
+      <div>
         {sharedGroup && sharedGroup.groupName
-          ? `공개된 그룹: ${sharedGroup.groupName}`
+          ? `그룹 ${sharedGroup.groupName}`
           : '비공개'}
       </div>
     </div>
