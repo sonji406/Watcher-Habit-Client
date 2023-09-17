feb2654f2bc8d2f216ad46ee7b63eb3ae60799b8
hamsterster <xhrrl003@gmail.com>
2023-09-18 08:09:10 +0900

    Feat: watcher 목록 조건부 렌더링 적용

---
+++ src/components/habits/HabitDetail/WatcherActions.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  subscribeWatcher,
+  unsubscribeWatcher,
+} from '../../../services/api/watcher';
+import { updateWatcherList } from '../../../redux/habitSlice';
+import getUserIdFromToken from '../../../utils/getUserIdFromToken';
+
+const WatcherActions = ({ habitDetail }) => {
+  const [hoveredWatcher, setHoveredWatcher] = useState(null);
+  const currentUserId = getUserIdFromToken();
+  let sortedApprovals = [];
+
+  const isCurrentUserCreator =
+    habitDetail.creator?._id?.toString() === currentUserId.toString();
+
+  const isCurrentUserWatcher = habitDetail.approvals?.some((approval) => {
+    return approval._id._id.toString() === currentUserId.toString();
+  });
+
+  const isGroupShared =
+    habitDetail.sharedGroup && habitDetail.sharedGroup.groupName;
+  const hasNoWatchers =
+    !habitDetail.approvals || habitDetail.approvals.length === 0;
+
+  if (habitDetail.approvals) {
+    sortedApprovals = [...habitDetail.approvals].sort((a, b) => {
+      if (a._id === currentUserId) return -1;
+      if (b._id === currentUserId) return 1;
+      return 0;
+    });
+  }
+
+  const dispatch = useDispatch();
+
+  const handleSubscribe = async () => {
+    try {
+      const habitId = habitDetail._id;
+      const watcherId = getUserIdFromToken();
+      const response = await subscribeWatcher(habitId, watcherId);
+      dispatch(updateWatcherList(response.updatedWatchers));
+    } catch (error) {
+      console.error('Failed to subscribe:', error);
+    }
+  };
+
+  const handleUnsubscribe = async () => {
+    try {
+      const habitId = habitDetail._id;
+      const watcherId = getUserIdFromToken();
+      const response = await unsubscribeWatcher(habitId, watcherId);
+      dispatch(updateWatcherList(response.updatedWatchers));
+    } catch (error) {
+      console.error('Failed to unsubscribe:', error);
+    }
+  };
+
+  const handleMouseEnter = (watcherId) => {
+    setHoveredWatcher(watcherId);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredWatcher(null);
+  };
+
+  return (
+    <div className='bg-main-bg p-4 rounded-lg mb-4 text-left'>
+      <span className='font-bold'>Watchers</span>
+      {isGroupShared ? (
+        <>
+          {hasNoWatchers ? (
+            <p className='text-center mb-4'>
+              내 습관을 구독한 <br />
+              Watcher가 없습니다
+            </p>
+          ) : (
+            <div className='flex flex-wrap mb-4'>
+              {!isCurrentUserWatcher && !isCurrentUserCreator && (
+                <div className='m-2'>
+                  <button
+                    className='rounded-full border border-gray-300 text-3xl bg-green-bg w-12 h-12 flex items-center justify-center'
+                    onClick={handleSubscribe}
+                  >
+                    +
+                  </button>
+                </div>
+              )}
+              {Array.isArray(sortedApprovals) &&
+                sortedApprovals.map((approval, index) => (
+                  <div
+                    className='m-2 relative'
+                    key={index}
+                    onMouseEnter={() => handleMouseEnter(approval._id)}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <img
+                      className='rounded-full border border-gray-300 w-12 h-12 object-cover'
+                      src={approval._id.profileImageUrl}
+                      alt='Watcher profile'
+                    />
+                    {hoveredWatcher === approval._id && (
+                      <button
+                        className='absolute top-0 right-0 text-xl bg-red-500 rounded-full w-6 h-6 flex items-center justify-center text-white'
+                        onClick={handleUnsubscribe}
+                      >
+                        -
+                      </button>
+                    )}
+                  </div>
+                ))}
+            </div>
+          )}
+        </>
+      ) : (
+        <p className='text-center mb-4'>
+          비공개 습관이므로
+          <br />
+          Watcher가 없습니다
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default WatcherActions;
