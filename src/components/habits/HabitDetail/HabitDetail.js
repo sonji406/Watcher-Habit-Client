34c8d00b6d7992ffccc821112b670785b2c4f99a
hamsterster <xhrrl003@gmail.com>
2023-09-20 16:59:15 +0900

    Feat: 수정 및 삭제 버튼 구현, 삭제 api 연결

--- src/components/habits/HabitDetail/HabitDetail.js
+++ src/components/habits/HabitDetail/HabitDetail.js
@@ -5,9 +5,28 @@ import HabitTime from './HabitTime';
 import HabitDaysOfWeek from './HabitDaysOfWeek';
 import HabitSection from './HabitSection';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import getUserIdFromToken from '../../../utils/getUserIdFromToken';
+import axios from 'axios';
 
 const HabitDetail = () => {
   const habitDetail = useSelector((state) => state.habit.habitDetail);
+
+  console.log(habitDetail);
+
+  const currentUserId = getUserIdFromToken();
+  const isCurrentUser = currentUserId === habitDetail.creator._id;
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(
+        `${process.env.REACT_APP_SERVER_DOMAIN}/api/habit/${habitDetail._id}`,
+      );
+      window.location.reload();
+    } catch (error) {
+      console.error('Habit deletion failed:', error);
+    }
+  };
 
   return (
     <div className='h-[calc(70vh-130px)] overflow-y-auto custom-scrollbar z-10 ml-4 mr-1.5 pr-1.5'>
@@ -28,6 +47,18 @@ const HabitDetail = () => {
       <HabitDaysOfWeek doDay={habitDetail.doDay} />
 
       <WatcherActions habitDetail={habitDetail} />
+      {isCurrentUser && (
+        <div>
+          <Link
+            to={{
+              pathname: `/edit-Habit/${habitDetail._id}`,
+            }}
+          >
+            수정
+          </Link>
+          <button onClick={handleDelete}>삭제</button>
+        </div>
+      )}
     </div>
   );
 };
