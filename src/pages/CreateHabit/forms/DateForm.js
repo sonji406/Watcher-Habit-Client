b5c3211756b18918adb934cc883119fc433c8e39
hamsterster <xhrrl003@gmail.com>
2023-09-23 22:33:37 +0900

    Refactor: 기한 선택 디자인 수정 및 validation 추가

--- src/pages/CreateHabit/forms/DateForm.js
+++ src/pages/CreateHabit/forms/DateForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const DateForm = ({
   habitStartDate,
@@ -7,37 +7,71 @@ const DateForm = ({
   setHabitEndDate,
 }) => {
   const today = new Date().toISOString().split('T')[0];
+  const [startDateValidation, setStartDateValidation] = useState('');
+  const [endDateValidation, setEndDateValidation] = useState('');
 
   const handleStartDateChange = (e) => {
     setHabitStartDate(e.target.value);
+
+    if (!e.target.value) {
+      setStartDateValidation('시작일을 선택해주세요.');
+    } else {
+      setStartDateValidation('');
+    }
+
+    if (habitEndDate && new Date(e.target.value) > new Date(habitEndDate)) {
+      setEndDateValidation('종료일은 시작일보다 이후여야 합니다.');
+    } else {
+      setEndDateValidation('');
+    }
   };
 
   const handleEndDateChange = (e) => {
     setHabitEndDate(e.target.value);
+
+    if (!e.target.value) {
+      setEndDateValidation('종료일을 선택해주세요.');
+    } else if (new Date(e.target.value) < new Date(habitStartDate)) {
+      setEndDateValidation('종료일은 시작일보다 이후여야 합니다.');
+    } else {
+      setEndDateValidation('');
+    }
   };
 
   return (
-    <div>
-      <label>기한</label>
-      <div className='grid grid-cols-2 gap-4 mb-4'>
-        <input
-          className='p-2 border rounded'
-          type='date'
-          value={habitStartDate}
-          min={today}
-          onChange={handleStartDateChange}
-          placeholder='Start Date'
-        />
-        <input
-          className='p-2 border rounded'
-          type='date'
-          value={habitEndDate}
-          min={habitStartDate}
-          onChange={handleEndDateChange}
-          placeholder='End Date'
-        />
+    <>
+      <label className='text-white ml-2'>
+        기한*
+        {startDateValidation && (
+          <span className='text-red-500 ml-2'>{startDateValidation}</span>
+        )}
+        {endDateValidation && (
+          <span className='text-red-500 ml-2'>{endDateValidation}</span>
+        )}
+      </label>
+      <div className='grid grid-cols-2 gap-4 mb-4 mt-2'>
+        <div>
+          <input
+            className='p-2 border-2 border-gray-500 shadow-lg rounded bg-dark-blue-bg text-white'
+            type='date'
+            value={habitStartDate}
+            min={today}
+            onChange={handleStartDateChange}
+            placeholder='Start Date'
+          />
+        </div>
+        <div>
+          <input
+            className='p-2 border-2 border-gray-500 shadow-lg rounded bg-dark-blue-bg text-white'
+            type='date'
+            value={habitEndDate}
+            min={habitStartDate}
+            onChange={handleEndDateChange}
+            placeholder='End Date'
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
