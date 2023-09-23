0fd645af620fba240b8e3680312b44b4549847af
hamsterster <xhrrl003@gmail.com>
2023-09-23 22:29:48 +0900

    Refactor: 기한 표현 수정

--- src/components/habits/HabitDetail/HabitDuration.js
+++ src/components/habits/HabitDetail/HabitDuration.js
@@ -4,19 +4,25 @@ const HabitDuration = ({ startDate: start, endDate: end }) => {
   const startDate = new Date(start);
   const endDate = new Date(end);
 
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  const formattedStartDate = startDate.toLocaleDateString('ko-KR', options);
+  const formattedEndDate = endDate.toLocaleDateString('ko-KR', options);
+
   const durationDays =
     Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24)) + 1;
 
   return (
-    <div className='bg-main-bg p-4 rounded-lg mb-4 w-1/3 mr-2'>
+    <div className='bg-main-bg p-4 rounded-lg mb-4 mr-2'>
       <span className='font-bold text-left block'>기한</span>
       <div className='text-center text-xl'>
-        {startDate.toLocaleDateString()}
+        {formattedStartDate} ~ {formattedEndDate}
+        <span className='ml-3 text-green-txt'>{durationDays}일 동안</span>
       </div>
-      <div className='text-center text-xl mb-4'>
-        ~ {endDate.toLocaleDateString()}
-      </div>
-      <div className='text-center text-xl mb-2'>{durationDays}일 동안</div>
     </div>
   );
 };
