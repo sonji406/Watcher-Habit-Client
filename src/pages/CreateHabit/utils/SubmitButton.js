f51eaaa9ea5efcb6d642cff22c55eac3029b2921
hamsterster <xhrrl003@gmail.com>
2023-09-23 22:35:44 +0900

    Refactor: submit 버튼 디자인 수정

--- src/pages/CreateHabit/utils/SubmitButton.js
+++ src/pages/CreateHabit/utils/SubmitButton.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import Loading from '../../../lib/loading/Loading';
 
-const SubmitButton = ({ isEdit, isLoading, handleSubmit }) => {
-  const buttonText = isEdit ? 'Edit' : 'Create';
+const SubmitButton = ({ isEdit, isLoading, handleSubmit, error }) => {
+  const buttonText = isEdit ? '수정' : '생성';
 
   return (
-    <button
-      disabled={isLoading}
-      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
-        isLoading ? 'cursor-not-allowed' : ''
-      }`}
-      onClick={handleSubmit}
-    >
-      {isLoading ? 'Loading...' : buttonText}
-    </button>
+    <div>
+      {error && (
+        <div className='text-red-500 mb-2'>
+          필수 항목*을 모두 작성해야 합니다.
+        </div>
+      )}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <button
+          disabled={isLoading}
+          className={`mr-2 bg-green-bg border-2 hover:bg-green-800 text-white font-bold py-2 px-4 rounded-lg ${
+            isLoading ? 'cursor-not-allowed' : ''
+          }`}
+          onClick={handleSubmit}
+        >
+          {buttonText}
+        </button>
+      )}
+    </div>
   );
 };
 
