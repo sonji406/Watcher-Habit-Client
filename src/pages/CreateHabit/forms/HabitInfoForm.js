a2ec526a1bec281ff78737b190468ef16b7dc13e
hamsterster <xhrrl003@gmail.com>
2023-09-23 22:34:03 +0900

    Refactor: 제목 및 내용 작성 폼 디자인 수정 및 validation 추가

--- src/pages/CreateHabit/forms/HabitInfoForm.js
+++ src/pages/CreateHabit/forms/HabitInfoForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const HabitInfoForm = ({
   habitTitle,
@@ -6,36 +6,64 @@ const HabitInfoForm = ({
   habitContent,
   setHabitContent,
 }) => {
+  const [habitTitleValidation, setHabitTitleValidation] = useState('');
+  const [habitContentValidation, setHabitContentValidation] = useState('');
+
   const handleHabitTitleChange = (e) => {
     setHabitTitle(e.target.value);
+
+    if (e.target.value === '') {
+      setHabitTitleValidation('습관 제목을 입력해 주세요.');
+    } else if (e.target.value.length < 2 || e.target.value.length > 10) {
+      setHabitTitleValidation(
+        '습관 제목은 2자 이상 10자 이내로 입력해 주세요.',
+      );
+    } else {
+      setHabitTitleValidation('');
+    }
   };
 
   const handleHabitContentChange = (e) => {
     setHabitContent(e.target.value);
+
+    if (e.target.value === '') {
+      setHabitContentValidation('습관 내용을 입력해 주세요.');
+    } else if (e.target.value.length < 2 || e.target.value.length > 100) {
+      setHabitContentValidation(
+        '습관 내용은 2자 이상 100자 이내로 입력해 주세요.',
+      );
+    } else {
+      setHabitContentValidation('');
+    }
   };
 
   return (
     <>
-      <label>습관 제목</label>
-      <div className='mb-4'>
+      <label className='text-white ml-2'>
+        제목* <span className='text-red-500 ml-2'>{habitTitleValidation}</span>
+      </label>
+      <div className='mb-6 mt-2'>
         <input
-          className='w-full p-2 border rounded'
+          className='w-full p-2 border-2 border-gray-500 shadow-lg rounded bg-dark-blue-bg text-white'
           type='text'
           value={habitTitle}
           onChange={handleHabitTitleChange}
-          placeholder='습관 제목을 입력하세요(최대 10자)'
+          placeholder='습관 제목을 입력하세요 (최대 10자)'
           minLength={2}
           maxLength={10}
         />
       </div>
 
-      <label>습관 내용</label>
-      <div className='mb-4'>
+      <label className='text-white ml-2'>
+        내용*
+        <span className='text-red-500 ml-2'>{habitContentValidation}</span>
+      </label>
+      <div className='mb-6 mt-2'>
         <textarea
-          className='w-full p-2 border rounded'
+          className='w-full p-2 border-2 border-gray-500 shadow-lg rounded bg-dark-blue-bg text-white'
           value={habitContent}
           onChange={handleHabitContentChange}
-          placeholder='습관 내용을 입력하세요(최대 100자)'
+          placeholder='습관 내용을 입력하세요 (최대 100자)'
           minLength={2}
           maxLength={100}
         />
