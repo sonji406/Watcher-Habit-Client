57798d55174f83ac8f727cfee25324f6c97b57b6
hamsterster <xhrrl003@gmail.com>
2023-09-23 22:31:15 +0900

    Refactor: useValidationForm 수정

--- src/hooks/useValidationForm.js
+++ src/hooks/useValidationForm.js
@@ -1,90 +1,66 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useValidation = () => {
   const [validationMessage, setValidationMessage] = useState('');
+  const [timeoutId, setTimeoutId] = useState(null);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [timeoutId]);
+
+  const displayValidationMessage = (message) => {
+    setValidationMessage(message);
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    const id = setTimeout(() => {
+      setValidationMessage('');
+    }, 3000);
+
+    setTimeoutId(id);
+  };
 
   const validateForm = ({
     habitTitle,
     habitContent,
-    habitStartDate,
-    habitEndDate,
     doDay,
     startTime,
     duration,
     minApprovalCount,
     sharedGroup,
     penalty,
+    habitStartDate,
+    habitEndDate,
   }) => {
-    if (!habitTitle) {
-      setValidationMessage('습관 제목을 입력해 주세요.');
+    if (
+      !habitTitle ||
+      !habitContent ||
+      !doDay.length ||
+      !startTime ||
+      duration === 0 ||
+      !penalty ||
+      !habitStartDate ||
+      !habitEndDate
+    ) {
+      displayValidationMessage('필수 항목*이 누락되었습니다.');
       return false;
     }
 
-    if (habitTitle.length < 2 || habitTitle.length > 10) {
-      setValidationMessage('습관 제목은 2자 이상 10자 이내로 입력해 주세요.');
+    if (
+      sharedGroup &&
+      (minApprovalCount === undefined || minApprovalCount <= 0)
+    ) {
+      displayValidationMessage('필수 항목*이 누락되었습니다.');
       return false;
     }
 
-    if (!habitContent) {
-      setValidationMessage('습관 내용을 입력해 주세요.');
-      return false;
-    }
-
-    if (habitContent.length < 2 || habitContent.length > 100) {
-      setValidationMessage('습관 내용은 2자 이상 100자 이내로 입력해 주세요.');
-      return false;
-    }
-
-    if (!habitStartDate) {
-      setValidationMessage('시작일을 선택해 주세요.');
-      return false;
-    }
-
-    if (!habitEndDate) {
-      setValidationMessage('종료일을 선택해 주세요.');
-      return false;
-    }
-
-    if (new Date(habitEndDate) < new Date(habitStartDate)) {
-      setValidationMessage('종료일은 시작일 이후로 설정해 주세요.');
-      return false;
-    }
-
-    if (!doDay.length) {
-      setValidationMessage('반복 주기를 선택해 주세요.');
-      return false;
-    }
-
-    if (!startTime) {
-      setValidationMessage('습관 시작 시간을 선택해 주세요.');
-      return false;
-    }
-
-    if (duration === 0) {
-      setValidationMessage('습관 진행 시간을 선택해 주세요.');
-      return false;
-    }
-
-    if (!penalty) {
-      setValidationMessage('패널티를 입력해 주세요.');
-      return false;
-    }
-
-    if (penalty.length < 2 || penalty.length > 50) {
-      setValidationMessage('패널티는 2자 이상 50자 이내로 입력해 주세요.');
-      return false;
-    }
-
-    if (sharedGroup) {
-      if (minApprovalCount <= 0) {
-        setValidationMessage(
-          '그룹 공유 시 최소 승인 인원은 1명 이상이어야 합니다.',
-        );
-        return false;
-      }
-    }
-
-    setValidationMessage('');
+    displayValidationMessage('');
     return true;
   };
 
