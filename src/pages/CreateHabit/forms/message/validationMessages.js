ed590836fd48e7aaefd48019ffc4a4475128c981
hamsterster <xhrrl003@gmail.com>
2023-09-24 13:20:47 +0900

    Refactor: validationMessages 파일에 검증 메시지 모음

---
+++ src/pages/CreateHabit/forms/message/validationMessages.js
@@ -0,0 +1,19 @@
+const validationMessages = {
+  startDateEmpty: '시작일을 선택해주세요.',
+  endDateAfterStart: '종료일은 시작일보다 이후여야 합니다.',
+  endDateEmpty: '종료일을 선택해주세요.',
+  habitTitleEmpty: '습관 제목을 입력해 주세요.',
+  habitTitleLength: '습관 제목은 2자 이상 10자 이내여야 합니다.',
+  habitContentEmpty: '습관 내용을 입력해 주세요.',
+  habitContentLength: '습관 내용은 2자 이상 100자 이내여야 합니다.',
+  minApprovalCountGroupShare:
+    '그룹 공유 시 최소 승인 인원은 1명 이상이어야 합니다.',
+  penaltyEmpty: '패널티를 입력해 주세요.',
+  penaltyLength: '패널티는 2자 이상 50자 이내여야 합니다.',
+  repeatEmpty: '반복 주기를 선택해 주세요.',
+  timeEmpty: '습관 시작 시간을 선택해 주세요.',
+  durationEmpty: '습관 진행 시간을 선택해 주세요.',
+  generalRequired: '필수 항목*을 모두 작성해야 합니다.',
+};
+
+export default validationMessages;
