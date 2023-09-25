1216d60e855b500750b3eac91da044090b100409
hamsterster <xhrrl003@gmail.com>
2023-09-26 00:35:45 +0900

    Fix: 히스토리 날짜 시차 적용

--- src/components/modals/weeklySchedule/WeeklyHabitList.js
+++ src/components/modals/weeklySchedule/WeeklyHabitList.js
@@ -7,6 +7,7 @@ yesterday.setDate(yesterday.getDate() - 1);
 yesterday.setHours(0, 0, 0, 0);
 
 const WeeklyHabitList = ({ habits, historyHabits, date }) => {
+  console.log(date);
   const isAfterYesterday = date > yesterday;
 
   const getStatusBorderColor = (status) => {
@@ -79,8 +80,10 @@ const WeeklyHabitList = ({ habits, historyHabits, date }) => {
     }, [])
     .sort((a, b) => a.startTime.localeCompare(b.startTime));
 
-  const currentDateHistoryHabits =
-    historyHabits[date.toISOString().split('T')[0]] || [];
+  const currentDateKey = `${date.getFullYear()}-${String(
+    date.getMonth() + 1,
+  ).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+  const currentDateHistoryHabits = historyHabits[currentDateKey] || [];
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedHabitImage, setSelectedHabitImage] = useState(null);
@@ -123,7 +126,7 @@ const WeeklyHabitList = ({ habits, historyHabits, date }) => {
       {currentDateHistoryHabits.map((habitHistory) => (
         <ul
           key={habitHistory.habitTitle}
-          className={`mb-1 text-white p-2 m-1 bg-main-bg rounded-lg break-words border-2 cursor-pointer transform transition-transform hover:scale-105 hover:bg-opacity-70 ${getStatusBorderColor(
+          className={`relative mb-1 text-white p-2 m-1 bg-main-bg rounded-lg break-words border-2 cursor-pointer transform transition-transform hover:scale-105 hover:bg-opacity-70 ${getStatusBorderColor(
             habitHistory.status,
           )}`}
           onClick={() =>
@@ -143,7 +146,7 @@ const WeeklyHabitList = ({ habits, historyHabits, date }) => {
         <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'>
           <div
             ref={modalRef}
-            className='relative bg-main-bg rounded-lg overflow-hidden max-w-lg w-full'
+            className='relative bg-main-bg rounded-lg overflow-hidden max-w-lg w-[500px] h-[350px] border-2 border-black'
           >
             <button
               className='absolute top-2 right-2 text-white rounded-full py-1 px-3 hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300'
@@ -152,23 +155,25 @@ const WeeklyHabitList = ({ habits, historyHabits, date }) => {
               <span className='relative top-[-2px]'>x</span>
             </button>
             <div className='p-4'>
-              <p className='text-xl text-center font-bold text-white mb-4'>
-                인증샷
+              <p className='text-xl text-center font-bold text-green-txt mb-4'>
+                인증 사진
               </p>
               <div className='text-center text-lg mb-2'>
                 {getStatusMessage(selectedHabitStatus)}
               </div>
-              {selectedHabitImage ? (
-                <img
-                  src={selectedHabitImage}
-                  alt='Selected Habit'
-                  className='w-full h-auto rounded-lg'
-                />
-              ) : (
-                <p className='text-center text-gray-400 m-20'>
-                  인증샷이 없습니다
-                </p>
-              )}
+              <div className='flex justify-center items-center'>
+                {selectedHabitImage ? (
+                  <img
+                    src={selectedHabitImage}
+                    alt='Selected Habit'
+                    className='w-[220px] h-[220px] object-cover rounded-2xl border'
+                  />
+                ) : (
+                  <p className='flex w-[220px] h-[220px] bg-gray-700 border rounded-2xl justify-center items-center text-center text-gray-400 mt-2'>
+                    인증 사진이 없습니다
+                  </p>
+                )}
+              </div>
             </div>
           </div>
         </div>
