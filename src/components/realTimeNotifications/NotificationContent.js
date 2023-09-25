cf3c2b9a0bcc308c17387057bdd58dbd32664342
sonji406 <wkaqh24@gmail.com>
2023-09-26 00:41:08 +0900

    Feat: 알림창 줄정리

--- src/components/realTimeNotifications/NotificationContent.js
+++ src/components/realTimeNotifications/NotificationContent.js
@@ -1,12 +1,17 @@
-const NotificationContent = ({ content }) => (
-  <>
-    {content.split('.').map((part, index) => (
-      <span key={index}>
-        {part}
-        <br />
-      </span>
-    ))}
-  </>
-);
+const NotificationContent = ({ content }) => {
+  const parts = content.split('.').filter((part) => part.trim() !== '');
+
+  return (
+    <>
+      {parts.map((part, index) => (
+        <span key={index}>
+          {part.trim()}
+          {index === 0 && '.'}
+          <br />
+        </span>
+      ))}
+    </>
+  );
+};
 
 export default NotificationContent;
