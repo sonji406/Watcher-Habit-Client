d5afa7ee9123c3a5a40c9a393306d0b540c204ea
sonji406 <wkaqh24@gmail.com>
2023-09-26 00:38:09 +0900

    Feat: 실시간 알림창 CSS 작업

--- src/components/realTimeNotifications/ActionButton.js
+++ src/components/realTimeNotifications/ActionButton.js
@@ -2,7 +2,7 @@ import getButtonText from '../../lib/notification/getButtonText';
 
 const ActionButton = ({ status, onClick }) => (
   <button
-    className='mx-auto text-white bg-green-400 py-1 px-2 rounded-lg hover:bg-green-500 focus:outline-none'
+    className='mx-auto text-white bg-green-400 py-1 px-2 font-semibold rounded-lg hover:bg-green-500'
     onClick={onClick}
   >
     {getButtonText(status)}
