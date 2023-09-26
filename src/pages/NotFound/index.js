a308c81226e184492291d0987bf8505fce6ea660
hamsterster <xhrrl003@gmail.com>
2023-09-26 12:48:03 +0900

    Refactor: 404 페이지 디자인

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -12,11 +12,31 @@ function NotFound() {
   };
 
   return (
-    <div>
-      <h1>404 error!</h1>
-      <h2>페이지를 찾을 수 없습니다.</h2>
-      <button onClick={goToHome}>홈으로 돌아가기</button>
-      <button onClick={goBack}>뒤로가기</button>
+    <div className='flex flex-col items-center justify-center h-screen bg-main-bg'>
+      <h1 className='text-white mb-10 text-5xl'>
+        페이지를 찾을 수 없습니다! 🧐
+      </h1>
+      <h2 className='text-3xl text-gray-400 mb-10'>404 Not Found</h2>
+      <p className='text-xl text-gray-400 mb-2'>
+        존재하지 않거나, 사용할 수 없는 페이지입니다
+      </p>
+      <p className='text-xl text-gray-400 mb-10'>
+        입력하신 주소가 정확한지 다시 확인해주세요
+      </p>
+      <div className='flex space-x-4'>
+        <button
+          onClick={goToHome}
+          className='bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-6 rounded m-2'
+        >
+          메인 화면으로
+        </button>
+        <button
+          onClick={goBack}
+          className='bg-gray-300 hover:bg-gray-400 text-gray-700 font-medium py-2 px-6 rounded m-2'
+        >
+          이전 화면으로
+        </button>
+      </div>
     </div>
   );
 }
