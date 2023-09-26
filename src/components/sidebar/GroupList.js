67dcf1a96901f9b0197e8defbbf5972955441ee3
hamsterster <xhrrl003@gmail.com>
2023-09-26 16:50:49 +0900

    Refactor: 가입된 그룹 없을 때 텍스트 추가

--- src/components/sidebar/GroupList.js
+++ src/components/sidebar/GroupList.js
@@ -9,20 +9,28 @@ const GroupList = ({ groupList }) => {
   const groupNames = groupList.groupNames;
   const groupIds = groupList.groupIds;
 
+  const hasNoGroups = groupNames.length === 0;
+
   return (
     <ul className='border-l-2 border-customDarkGray border-solid py-0.5 ml-6 mt-6 overflow-y-auto max-h-40 transparent-scrollbar overflow-x-hidden'>
-      {groupNames.map((groupName, index) => {
-        const groupId = groupIds[index];
+      {hasNoGroups ? (
+        <div className='text-center py-4 ml-3 text-customDarkGray'>
+          가입된 그룹이 없습니다
+        </div>
+      ) : (
+        groupNames.map((groupName, index) => {
+          const groupId = groupIds[index];
 
-        return (
-          <Group
-            key={groupId}
-            groupId={groupId}
-            pathGroupId={pathGroupId}
-            groupName={groupName}
-          />
-        );
-      })}
+          return (
+            <Group
+              key={groupId}
+              groupId={groupId}
+              pathGroupId={pathGroupId}
+              groupName={groupName}
+            />
+          );
+        })
+      )}
     </ul>
   );
 };
