0cc1271b567fe28781b1b5bd5769cbbc81861f82
sonji406 <wkaqh24@gmail.com>
2023-10-08 16:28:08 +0900

    Feat: 리프래시 토큰 만료 로그아웃

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,33 +7,33 @@ const api = axios.create({
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
-    if (
-      error.response &&
-      error.response.status === 401 &&
-      !error.config._retry
-    ) {
-      error.config._retry = true;
+    if (error.response && error.response.status === 401) {
+      if (error.response.data.error === '리프레시 토큰이 만료되었습니다.') {
+        window.location.href = '/';
+        return Promise.reject(error);
+      }
 
-      try {
-        const res = await api.post(
-          `${process.env.REACT_APP_SERVER_DOMAIN}/api/auth/refreshToken`,
-          {},
-          { withCredentials: true },
-        );
+      if (!error.config._retry) {
+        error.config._retry = true;
+        try {
+          const res = await api.post(
+            `${process.env.REACT_APP_SERVER_DOMAIN}/api/auth/refreshToken`,
+            {},
+            { withCredentials: true },
+          );
 
-        const newAccessToken = res.data.accessToken;
-        api.defaults.headers.common[
-          'Authorization'
-        ] = `Bearer ${newAccessToken}`;
+          const newAccessToken = res.data.accessToken;
+          api.defaults.headers.common[
+            'Authorization'
+          ] = `Bearer ${newAccessToken}`;
+          error.config.headers['Authorization'] = `Bearer ${newAccessToken}`;
 
-        error.config.headers['Authorization'] = `Bearer ${newAccessToken}`;
-
-        return api(error.config);
-      } catch (refreshError) {
-        return Promise.reject(refreshError);
+          return api(error.config);
+        } catch (refreshError) {
+          return Promise.reject(refreshError);
+        }
       }
     }
-
     return Promise.reject(error);
   },
 );
