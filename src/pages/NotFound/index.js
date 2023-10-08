73f16af4f680871a82e133d41485e1e956613902
hamsterster <xhrrl003@gmail.com>
2023-10-08 13:56:33 +0900

    Refactor: 404 페이지에 마이페이지 이동 버튼 추가

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,17 +1,24 @@
 import { useNavigate } from 'react-router-dom';
 import { useDocumentTitle } from '../../hooks/useDocumentTitle';
+import getUserIdFromToken from '../../utils/getUserIdFromToken';
+import { useFetchUserInfo } from '../../hooks/useFetchUserInfo';
 
 function NotFound() {
   const navigate = useNavigate();
 
   useDocumentTitle('404 - 페이지를 찾을 수 없습니다');
 
+  const userId = getUserIdFromToken();
+  const userNickname = useFetchUserInfo(userId);
+
   const goToHome = () => {
     navigate('/');
   };
 
-  const goBack = () => {
-    navigate(-1);
+  const goToMyHabitPage = () => {
+    if (userNickname) {
+      navigate(`/my-habit/${userNickname}`);
+    }
   };
 
   return (
@@ -26,20 +33,18 @@ function NotFound() {
       <p className='text-xl text-gray-400 mb-10'>
         입력하신 주소가 정확한지 다시 확인해주세요
       </p>
-      <div className='flex space-x-4'>
-        <button
-          onClick={goToHome}
-          className='bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-6 rounded m-2'
-        >
-          메인 화면으로
-        </button>
-        <button
-          onClick={goBack}
-          className='bg-gray-300 hover:bg-gray-400 text-gray-700 font-medium py-2 px-6 rounded m-2'
-        >
-          이전 화면으로
-        </button>
-      </div>
+      <button
+        onClick={goToMyHabitPage}
+        className='bg-blue-500 hover:bg-blue-600 text-white font-bold text-lg py-4 px-6 rounded m-2'
+      >
+        나의 습관 관리 페이지로 이동
+      </button>
+      <button
+        onClick={goToHome}
+        className='hover:text-green-500 text-gray-500 font-medium py-2 px-4 rounded mt-5'
+      >
+        로그인 화면으로 이동
+      </button>
     </div>
   );
 }
